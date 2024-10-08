0fc29a506824ce73f510861c0088d00be6d843d0
Audrey Gu <audreymgu@gmail.com>
2024-10-08 10:41:04 -0400

    Sveltekit integration started.

---
+++ watch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CSS_FILE="/Users/gu/Repositories/chief-sveltekit/src/game/main.css"
+TWEET_FILE="/Users/gu/Repositories/chief-sveltekit/src/game/special/StoryStylesheet.twee"
+
+update_twee_file() {
+  echo "::Story Stylesheet" [stylesheet]> "$TWEET_FILE"
+  cat "$CSS_FILE" >> "$TWEET_FILE"
+  echo "Updated $TWEET_FILE with latest CSS"
+}
+
+fswatch -o "$CSS_FILE" | while read event
+do
+  update_twee_file
+done

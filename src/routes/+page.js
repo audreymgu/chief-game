b03ded3188124f8068aa973c86de6cfd5d167a2a
Audrey Gu <audreymgu@gmail.com>
2024-11-07 17:30:11 -0500

    Update OG graph copy.

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -7,39 +7,39 @@ export function load({ url }) {
     // Set dynamic Open Graph tags or other content based on the result
     if (result === 'fri') {
         ogTitle = "👏👏 NOT HERE TO MAKE FRIENDS 👏👏 - The Corporate Trail";
-        ogDescription = "Water cooler talk was never your thing. You’d prefer coworkers not bother you, but if they must, they need to make it actionable.";
+        ogDescription = "I’m efficient, strategic, and I’ve kept distractions at bay. I’d prefer coworkers not bother me, but if they must, they need to make it actionable.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-friends_share.jpg';
     } else if (result === 'bos') {
         ogTitle = "🤝🧳 I MANAGE LIKE A BOSS 🤝🧳 - The Corporate Trail";
-        ogDescription = "You have an innate ability to herd your colleagues toward a common goal. Have you considered starting a cult?";
+        ogDescription = "I have an innate ability to inspire my colleagues and herd them toward a common goal.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-boss_share.jpg';
     } else if (result === 'bel') {
         ogTitle = "💗💗 I AM BELOVED 💗💗 - The Corporate Trail";
-        ogDescription = "People like working with you. They tend to think you’re on their side—and they’re right. You probably get hit up on LinkedIn a lot.";
+        ogDescription = "People trust that I’ve got their back. My calendar tends to fill up with gratuitous coffee chats and one-on-ones.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-beloved_share.jpg';
     } else if (result === 'out') {
         ogTitle = "🏃💨 JUST HEADING OUT 🏃💨 - The Corporate Trail";
-        ogDescription = "Oh, a bunch of stakeholders are jumping on a quick Teams to chat through action items? Sorry, you’ll have to circle back.";
+        ogDescription = "Oh, a last-minute Zoom meeting? Sorry, I’ll have to circle back. Some call it a healthy work-life balance, I call it self-preservation.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-out_share.jpg';
     } else if (result === 'vio') {
         ogTitle = "🔪🔪 I CHOSE VIOLENCE 🔪🔪 - The Corporate Trail";
-        ogDescription = "For you to win, someone else has to lose. Call it a competitive spirit. You either played D1 athletics or too much Monopoly.";
+        ogDescription = "For me to win, someone else has to lose. So what if I stab a teammate in the back? There are no teams on The Corporate Trail. ";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-violence_share.jpg';
     } else if (result === 'pro') {
         ogTitle = "💸🩸 I BLEED FOR PROFIT 💸🩸 - The Corporate Trail";
-        ogDescription = "You’re a hero of free market enterprise. You don’t stop until the work is done, and value is safely returned to shareholders.";
+        ogDescription = "I’ve hit revenue targets by missing weekends and weddings while pounding triple-shot espressos and gas station No-Doze.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-profit_share.jpg';
     } else if (result === 'gre') {
         ogTitle = "🧯🔥 I AM THE EXECUTIVE FIREFIGHTER 🧯🔥 - The Corporate Trail";
-        ogDescription = "You solve all sorts of problems — even the ones you have nothing to do with. People appreciate your dependability, but after fighting so many dumpster fires, you’re at risk of burning out.";
+        ogDescription = "I solve all sorts of problems — even the ones I have nothing to do with.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-fire_share.jpg';
     } else if (result === 'got') {
         ogTitle = "👑👑 I GAME OF THRONES'D IT 👑👑 - The Corporate Trail";
-        ogDescription = "Your feel for office politics is as honed as it is frightening. Maybe you should look into actual politics.";
+        ogDescription = "My feel for office politics is frighteningly sharp — I’ve outmaneuvered everyone and survived all power plays.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-thrones_share.jpg';
     } else if (result === 'qui') {
         ogTitle = "🦎🦎 I QUIT 🦎🦎 - The Corporate Trail";
-        ogDescription = "Spend your best years saying shit like “circle back” while drinking a watery K-Cup? No thanks. Time to abscond to Costa Rica and become an iguana tamer.";
+        ogDescription = "Spend the best years of my life “circling back” over stale office coffee? No thanks. Time to pack it up, blow it up, and move to Costa Rica.";
         ogImage = 'https://thecorporatetrail.chief.com/img/e-quit_share.jpg'; 
     } else {
         ogTitle = "THE CORPORATE TRAIL";
